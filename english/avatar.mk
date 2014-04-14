7e2e65304e1dd5ad5040dc84de5d3076f86b9390
Robin Moussu <robin.moussu+github@gmail.com>
2014-04-14 15:14:27 +0200

    cour 14 avril

---
+++ english/avatar.mk
@@ -0,0 +1,24 @@
+# Text File
+# AUTHOR:   robin
+# FILE:     avatar.mk
+# ROLE:     TODO (some explanation)
+
+make up
+ - everything have been explored, including the limitations (eg. body's proportions)
+
+solution : CG characters
+ - no physical limitation (gender, body's proportions, number of finger, size, skin's color – including transparent skin)
+ - allow unique creation of realistic alien spicies
+
+----
+
+For Avatar, Cameron wants to create realistic aliens.
+They developd a new "image-based facial performace capture" system, to record the facial expression of the actors. The add a camera on a headgear of the actors. The camera recorded with higher precesion that all have done before, including eye movement. Using that issue, the other camera had lesser constrain, and were used only to record body movement.
+With the benefit of thoses new techniques, the actors had less constraints than before, and there performace was recorded with higher fidelity.
+In addition, James Cameron used Virtual Camera. During the editing, he saw the CG characters instead of the actors. So virtual camera have no physical limitations, and he saw same thing that the peoples will see in the cinema.
+
+
+
+
+
+
